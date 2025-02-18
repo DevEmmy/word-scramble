b53affea3736d97662ba9f9d0afe17a714e43e51
DevEmmy <eolaosebikan60@gmail.com>
2025-02-18 22:09:51 +0100

    Refactor GameComponent to include state management for original word, scrambled word, user guess, message, and score

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 const wordList = ["web3", "solana", "ethereum", "bitcoin", "degen", "crypto"];
 
-const scrambleWord = (word: string) => word.split("").sort(() => Math.random() - 0.5).join("");
+const scrambleWord = (word: string) =>
+  word
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
 
 const GameComponent = () => {
-  return (
-    <div>
+  const [originalWord, setOriginalWord] = useState("");
+  const [scrambledWord, setScrambledWord] = useState("");
+  const [userGuess, setUserGuess] = useState("");
+  const [message, setMessage] = useState("");
+  const [score, setScore] = useState(0);
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default GameComponent
+export default GameComponent;
